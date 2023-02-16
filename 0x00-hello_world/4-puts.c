262c3451b8226ea3fb2bc9681acb2e4d6dab0080
mina <mr.safwat55@gmail.com>
2023-02-16 13:46:46 -0800

    solve c tasks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+/*
+ * file : 4-puts.c
+ * auth : mina safwat
+ */
+
+#include<stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+
+	puts("Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+/*
+ * File: 6-size.c
+ * Auth: mina safwat <mr.safwat55@gmail.com>
+ */
+
+#include<stdio.h>
+
+/**
+ * main - prints the size of different data types
+ *
+ * Return: always (0)
+ */
+
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of a int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
+
+}
