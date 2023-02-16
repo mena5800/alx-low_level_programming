948ea50d0140abc3a13d61ef3add243ef7cdfb58
mina <mr.safwat55@gmail.com>
2023-02-16 14:48:40 -0800

    solve last task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+/*
+ * file: 101-quote.c
+ * auth: mina safwat
+ */
+
+#include<stdio.h>
+#include<unistd.h>
+
+/**
+ * main - print "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ *
+ * Return: always (1)
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
