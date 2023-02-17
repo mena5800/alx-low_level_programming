97595606e2a7fc126afe6605896995775475e8e9
mina <mr.safwat55@gmail.com>
2023-02-17 11:18:15 -0800

    solve task 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,23 +1,32 @@
 /*
- * file: 2-print_alphabet.c
+ * file: 100-print_comb3.c
  * auth: mina safwat
  */
 
 #include<stdio.h>
 
 /**
- * main - prints the alphabet in lowercase, followed by a new line.
+ * main - prints all possible different combinations of two digits.
  * Return: always return 0
  */
 
 int main(void)
 {
-int start = 97;
-int end = 122;
-while (start <= end)
+int space = 32;
+int comma = 44;
+for (int i = 48; i < 57; i++)
 {
-	putchar(start);
-	start++;
+	for (int j = i + 1; j <= 57; j++)
+	{
+		putchar(i);
+		putchar(j);
+		if (i == 56 && j == 57)
+		{
+			break;
+		}
+		putchar(space);
+		putchar(comma);
+	}
 }
 putchar('\n');
 return (0);
