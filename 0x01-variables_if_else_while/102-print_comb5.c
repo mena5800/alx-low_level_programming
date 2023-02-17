f51d7b5651d6f2d0bfa7df18f90b98f22eb189ce
mina <mr.safwat55@gmail.com>
2023-02-17 10:20:39 -0800

    solve task 5, 6

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,24 @@
+/*
+ * file: 2-print_alphabet.c
+ * auth: mina safwat
+ */
+
+#include<stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, followed by a new line.
+ * Return: always return 0
+ */
+
+int main(void)
+{
+int start = 97;
+int end = 122;
+while (start <= end)
+{
+	putchar(start);
+	start++;
+}
+putchar('\n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+/*
+ * file: 5-print_numbers.c
+ * auth: mina safwat
+ */
+
+#include<stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 starting from 0
+ * , followed by a new line.
+ * Return: always return 0
+ */
+
+int main(void)
+{
+int start = 48;
+int end = 57;
+while (start <= end)
+{
+	putchar(start);
+	start++;
+}
+putchar('\n');
+return (0);
+}
