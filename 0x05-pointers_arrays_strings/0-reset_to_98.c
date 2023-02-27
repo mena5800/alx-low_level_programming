3e40fedf3e0bb6dbe483791aaae0df04ec1f833c
mina <mr.safwat55@gmail.com>
2023-02-27 13:35:43 -0800

    add all files and solve task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+/*
+ * auth: Mina Safwat
+ * file: 0-reset_to_98.c
+ */
+
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer and updates the value
+ * it points to 98.
+ * @n: the pointer.
+ * Return: void.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
