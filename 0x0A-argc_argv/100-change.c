787979f73105d5639dc490b76c8786f62046b301
mina <mr.safwat55@gmail.com>
2023-03-10 15:11:28 -0800

    fix task 100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -19,7 +19,11 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-
+	if (atoi(argv[1]) < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
 
 	i = atoi(argv[1]);
 	reminder = i;
