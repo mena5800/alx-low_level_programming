cdb6c9166e8fd9d08208465739c17159af5d730d
mina <mr.safwat55@gmail.com>
2023-03-14 17:09:42 -0700

    fix task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,7 +19,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int n = size;
 	unsigned int i;
 
-	if (size <= 0 && *c == '\0')
+	if (size <= 0 && c == '')
 		return (NULL);
 
 	arr = malloc(n);
