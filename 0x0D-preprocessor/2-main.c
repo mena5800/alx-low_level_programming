7cd5e1df714b45d3a0fdf672f5f4769247e5f83b
mina <mr.safwat55@gmail.com>
2023-03-20 14:36:36 -0700

    solve all tasks

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,18 @@
+/*
+ * auth : mina safwat samy
+ * file : 2-main.c
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file.
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
