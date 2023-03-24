d2088ad2c7cf0bbcb68ff61ee454c746cc42a535
mina <mr.safwat55@gmail.com>
2023-03-23 19:18:45 -0700

    solve task 2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,52 @@
+/*
+ * auth : mina safwat samy
+ * file : 2-print_strings.c
+ */
+
+#include "variadic_functions.h"
+
+/**
+ * print_strings - print all strings
+ * @separator: separator of numbers
+ * @n : the number of integars
+ * Return: void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ar;
+	unsigned int i;
+	char *word;
+
+	if (n == 0)
+		return;
+
+	va_start(ar, n);
+	if (separator != NULL)
+	{
+		for (i = 0; i < n - 1; i++)
+		{
+			word = va_arg(ar, char *);
+			if (word == NULL)
+				word = "(nil)";
+			printf("%s%s", word, separator);
+		}
+	}
+	else
+	{
+		for (i = 0; i < n - 1; i++)
+		{
+			word = va_arg(ar, char *);
+			if (word == NULL)
+				word = "(nil)";
+			printf("%s", word);
+		}
+	}
+
+	word = va_arg(ar, char *);
+	if (word == NULL)
+		word = "(nil)";
+	printf("%s\n", word);
+	va_end(ar);
+
+}
