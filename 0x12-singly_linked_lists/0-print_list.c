21b65c3ba044183a434bff4d13d58ee9bd49c71d
mina <mr.safwat55@gmail.com>
2023-03-30 17:41:28 -0700

    solve task0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include <lists.h>
+
+/**
+ * print_list - print the linked list
+ * @h: the head of linked list
+ * Return: the size of linked list
+ */
+size_t print_list(const list_t *h)
+{
+size_t counter = 0;
+
+if (h == NULL)
+	return (0);
+
+while (h)
+{
+if (h->str == NULL)
+	printf("[0] (nil)\n");
+else
+	printf("[%u] %s\n", h->len, h->str);
+
+h = h->next;
+counter++;
+}
+return (counter);
+}
