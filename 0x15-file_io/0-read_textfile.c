7cf6899408bae7249dcacd3ec124c64b73fe8685
mena5800 <mr.safwat55@gmail.com>
2023-05-09 05:26:31 +0300

    solve first problem

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * read_textfile - print the text from file
+ * @filename:filename var
+ * @letters:num of letters
+ * Return: size of text
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	size_t counter = 0;
+	char c;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+
+	fd = open(filename, O_RDWR);
+
+	if (fd == -1)
+	{
+		return (0);
+	}
+
+	while (read(fd, &c, 1) && (counter < letters))
+	{
+		if (write(1, &c, 1) == -1)
+		{
+			return (0);
+		}
+		counter++;
+	}
+	return (counter);
+}
