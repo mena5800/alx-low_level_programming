e1035262b6652a01ac733f475d814c4bcc25c4fa
mena5800 <mr.safwat55@gmail.com>
2023-06-17 08:55:41 +0300

    solve task 5 - 6

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,1 +1,21 @@
 #include "lists.h"
+/**
+ * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list.
+ * @head: the head of double linked list
+ * @index: the index of node.
+ * Return: node in index.
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	for (i = 0; i < index; i++)
+	{
+		head = head->next;
+		if (head == NULL)
+		{
+			return (NULL);
+		}
+	}
+	return (head);
+}
--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,1 +1,21 @@
 #include "lists.h"
+/**
+ * sum_dlistint - returns the sum of all the data of a dlistint_t linked list.
+ * @head: head of list
+ * Return: sum of all nodes in list.
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+	{
+		return (sum);
+	}
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
