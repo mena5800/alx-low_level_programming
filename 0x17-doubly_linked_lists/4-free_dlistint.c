3a9e470425e1143cba030fe1e765c23b380c6fc0
mena5800 <mr.safwat55@gmail.com>
2023-06-17 08:35:37 +0300

    solve 4

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,1 +1,17 @@
 #include "lists.h"
+/**
+ * free_dlistint - frees a dlistint_t list.
+ * @head: the head of double list
+ * Return: always void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp = head;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
